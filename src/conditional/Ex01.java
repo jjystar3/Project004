c800145dc7839dba46031cee151574d4dacae24a
Ju Young <jjystar3@gmail.com>
2024-06-26 15:59:18 +0900

    프로젝트 4 생성

---
+++ src/conditional/Ex01.java
@@ -0,0 +1,21 @@
+package conditional;
+
+public class Ex01 {
+
+	public static void main(String[] args) {
+		
+		//학생의 나이가 8살 이상이면 "학교에 다닙니다" 출력하기
+		int age = 10;
+		if (age >= 8) {
+			System.out.println("학교에 다닙니다");
+		}
+		
+		//블록의 의미
+		if (age >= 8) {
+			int temp = 0; //temp는 블록이 끝나기 전까지만 존재
+			System.out.println();
+		}
+		//System.out.println(temp); //변수가 소멸되어 에러남
+	}
+
+}
