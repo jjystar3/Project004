66e7ddf6645de585a97609fcbc2170564003893b
Ju Young <jjystar3@gmail.com>
2024-06-26 16:50:36 +0900

    조건문 - if문 예제 생성

---
+++ src/conditional/quiz/Quiz02.java
@@ -0,0 +1,16 @@
+package conditional.quiz;
+
+public class Quiz02 {
+
+	public static void main(String[] args) {
+		
+		int age = 26;
+		char gender = 'M';
+		
+		if(age >= 8 && gender == 'F') {
+			System.out.println("여학생입니다");
+		}
+		
+	}
+
+}
