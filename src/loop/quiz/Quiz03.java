4094f2a87db67014478edaa689c98f4da6a557ef
Ju Young <jjystar3@gmail.com>
2024-06-28 16:55:49 +0900

    for문 예제 업로드 2

--- src/loop/quiz/Quiz03.java
+++ src/loop/quiz/Quiz03.java
@@ -3,9 +3,13 @@ package loop.quiz;
 public class Quiz03 {
 
 	public static void main(String[] args) {
-		
-		
-		
+
+		int n = 10;
+
+		for (int i = 0; i < n; i++) {
+			System.out.print("*");
+		}
+
 	}
 
 }
