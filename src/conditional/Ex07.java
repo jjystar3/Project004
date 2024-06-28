d3cae733b569971e275b6e7378c25f91cfc13724
Ju Young <jjystar3@gmail.com>
2024-06-28 15:00:51 +0900

    while문 예제 업로드

--- src/conditional/Ex07.java
+++ src/conditional/Ex07.java
@@ -4,7 +4,21 @@ public class Ex07 {
 
 	public static void main(String[] args) {
 		
+		int time = 8;
 		
+		switch(time) {
+		case 8:
+			System.out.println("출근합니다.");
+			break;
+		case 9:
+			System.out.println("회의를 합니다.");
+			break;
+		case 10:
+			System.out.println("업무를 봅니다.");
+			break;
+		default:
+			System.out.println("외근을 나갑니다.");
+		}
 		
 	}
 
--- src/loop/Ex01.java
+++ src/loop/Ex01.java
@@ -4,8 +4,11 @@ public class Ex01 {
 
 	public static void main(String[] args) {
 		
-		
-		
+		int num=1;
+		while(num<=10) {
+			System.out.println("안녕하세요");
+			num++;
+		}
 	}
 
 }
